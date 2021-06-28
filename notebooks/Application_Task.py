29bdef5362bc682666ae4a5a6fcebcf485a856b0
Jordi Planas <jordi.planasc@gmail.com>
2021-06-28 12:04:14 +0200

    Update Application_Task.py

--- notebooks/Application_Task.py
+++ notebooks/Application_Task.py
@@ -4,5 +4,5 @@ import nltk
 
 if __name__ == '__main__':
     df = pd.read_csv('../input/20210103_hundenamen.csv')
-    names = [name for name in df['HUNDENAME'] if nltk.edit_distance(name, 'Luca') == 1]
+    names = [name for name in df.iloc[:, 0] if nltk.edit_distance(name, 'Luca') == 1]
     print(','.join(set(names)))
